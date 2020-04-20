f3d35c32e92094025dda7eeccd7d7c76565a348a
sherinsteve <sherinsteve@users.noreply.github.com>
2020-04-19 23:18:13 -0700

    Updated main

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ class Main {
 
     public static void main(String [] args)
     {
-        System.out.println("Greeeeeen narwals");
+        System.out.println("Greeeeeen narwals: Nima & Sherin");
     }
 
 }
